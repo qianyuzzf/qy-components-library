de8f20ff824a738ad96fc990b6250f6a121d16b7
RS\zuozf <280624745@qq.com>
2024-12-27 13:50:33 +0800

    新建监听浏览器窗口的宽度变化hook

---
+++ src/hooks/useWindowWidth.js
@@ -0,0 +1,23 @@
+import { ref, onMounted, onUnmounted } from 'vue'
+import _ from 'lodash'
+
+const useWindowWidth = (debounceDelay = 300) => {
+  const screenWidth = ref(window.innerWidth)
+
+  const updateScreenWidth = _.debounce(() => {
+    screenWidth.value = window.innerWidth
+  }, debounceDelay)
+
+  onMounted(() => {
+    window.addEventListener('resize', updateScreenWidth)
+  })
+
+  onUnmounted(() => {
+    window.removeEventListener('resize', updateScreenWidth)
+    updateScreenWidth.cancel() // 防止组件卸载后未清理的定时器
+  })
+
+  return screenWidth
+}
+
+export default useWindowWidth
